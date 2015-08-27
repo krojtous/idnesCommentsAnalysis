bde779628325cfab3e1bdc77c4f490b915da37e0
Matous Pilnacek <matousp@gmail.com>
2015-08-27 21:03:40 +0200

    analyze groups added

--- analyzeGroups.R
+++ analyzeGroups.R
@@ -1,1 +1,26 @@
 #analyzeGroups.R
+
+#---------------------------------------analyzeGroups-------------------------------------
+analyzeGroups = function( relations, comments ){
+    
+    #----main function for analyzing groups
+    require(igraph)
+    findGroups( relations )
+  
+}
+#--------------------------------------findGroups-----------------------------------------
+findGroups = function ( relations ){
+    
+    relationsW = as.matrix(relations)
+    network    = graph.data.frame(relations, directed=F)
+    wc         = walktrap.community( network )
+    wc         = cluster_spinglass( network, spins = 3 )
+    in.deg = degree(network,v=V(network), mode="in")
+    membership(wc)
+    is_hierarchical(wc)
+    #vykresleni grafu
+    #png(filename="/media/matous/A4E0A47DE0A456F8/Dokumenty/Sociologie/Diplomová práce/group_graph.png")
+    plot(wc,network,  vertex.label=NA, vertex.size=log(in.deg)*2, edge.color = "black", edge.width=E(network)$weight/2,
+        main = "Graph of relations in duscussion with groups")
+    #dev.off()
+}
--- main.r
+++ main.r
@@ -1,27 +1,28 @@
 #main.r
 #script loads, select, analyze data for one dataset (month)
 
-#----------------------------load packages (move in right files)--------------------------------
+#----------------------------REQUIRED PACKAGES--------------------------------
 require(igraph)
 require(plyr)
 
-#----------------------------load functions-----------------------------------------------------
+#----------------------------LOAD FUNCTIONS-----------------------------------------------------
 source("/media/matous/A4E0A47DE0A456F8/Dokumenty/R/idnesCommentsAnalysis/selectData.R")
 source("/media/matous/A4E0A47DE0A456F8/Dokumenty/R/idnesCommentsAnalysis/transformData.R")
 source("/media/matous/A4E0A47DE0A456F8/Dokumenty/R/idnesCommentsAnalysis/analyzeGroups.R")
 
-#----------------------------load data----------------------------------------------------------
+#----------------------------LOAD DATA----------------------------------------------------------
 relations = read.csv("/media/matous/A4E0A47DE0A456F8/Dokumenty/Sociologie/Diplomová práce/Data/Relations/relations_2015_07.csv")
 comments = read.csv("/media/matous/A4E0A47DE0A456F8/Dokumenty/Sociologie/Diplomová práce/Data/Relations/comments_2015_07.csv")
+relationsBackup = relations
 
-
-#----------------------------select and transform data---------------------------------------------
-relations = selectData ( relations, 1000, "zahranicni" )
+#----------------------------SELECT AND TRANSFROM DATA---------------------------------------------
+relations = selectData ( relations, 500, "zahranicni" )
 relations = transformData ( relations )
 
-#----------------------------analyze data-------------------------------------------------------
-#TO DO
+#----------------------------ANALYZE DATA-------------------------------------------------------
+basicResults = analyzeBasic ( relations, comments )
+groupResults = analyzeGroups( relations, comments )
 
-#----------------------------display data-------------------------------------------------------
+#----------------------------EXPORT DATA-------------------------------------------------------
 #TO DO
 file.choose()
